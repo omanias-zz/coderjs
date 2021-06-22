35fc4a22b03a5457db8189718b9a3a9f11084454
Omar Jesús Maniás <ojmanias1985@gmail.com>
2021-06-22 20:26:20 -0300

    Agrego contenido de clase 3

---
+++ scripts/app3.js
@@ -0,0 +1,7 @@
+let numero1 = Number(prompt("Introduce un número"));
+let numero2 = Number(prompt("Introduce otro número"));
+
+for (let i = numero1; i <= numero2; i++) {
+
+    console.log(`del ${numero1} hasta el ${numero2}, está el: ${i}`);
+}
